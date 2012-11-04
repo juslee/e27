691e69e95bfdb72c41a148e6d9ccf16f450a7587
Jairus Bondoc <jairus@nmgresources.ph>
2012-11-04 18:45:54 +0800

    Just changed ordering of submenus

--- app/application/views/blogs_rss/submenus.php
+++ app/application/views/blogs_rss/submenus.php
@@ -8,11 +8,11 @@
 			<li <?php if($method=="all"){ echo "class='selected'"; } ?> onclick='self.location="<?php echo site_url()."blogs_rss/all";?>"'>
 				All
 			</li>
-			<li <?php if($method=="people"){ echo "class='selected'"; } ?> onclick='self.location="<?php echo site_url()."blogs_rss/people";?>"'>
-				People
-			</li>
 			<li <?php if($method=="companies"){ echo "class='selected'"; } ?> onclick='self.location="<?php echo site_url()."blogs_rss/companies";?>"'>
 				Companies
+			</li>
+			<li <?php if($method=="people"){ echo "class='selected'"; } ?> onclick='self.location="<?php echo site_url()."blogs_rss/people";?>"'>
+				People
 			</li>
 			<li <?php if($method=="investment_orgs"){ echo "class='selected'"; } ?> onclick='self.location="<?php echo site_url()."blogs_rss/investment_orgs";?>"'>
 				Investment Organizations
