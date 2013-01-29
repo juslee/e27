46c0158cf44ffaf9f0552dd18def2c50b055e942
Jairus <jairus@nmgresources.ph>
2013-01-29 14:13:28 +0800

    feedback email on footer

--- app/application/views/startuplist/footer.php
+++ app/application/views/startuplist/footer.php
@@ -1,4 +1,7 @@
 <a href='<?php echo site_url(); ?>companylist'>Companies</a>  <a href='<?php echo site_url(); ?>personlist'>People</a>  <a href='<?php echo site_url(); ?>investment_orglist'>Investment Organizations</a>
 <br />
 <br />
+<div style='color:black'>
+For comments and suggestions please email us at <a href='mailto:feedback@27x.co' style='color:black' >feedback@27x.co</a><br />
 Copyright &copy; <?php echo date("Y"); ?> 27x.co
+</div>
